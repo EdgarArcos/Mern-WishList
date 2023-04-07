a40d098a6b52b264c5c69b4f535c04f156a08878
Edgar Arcos <edgararcosmartin@gmail.com>
2023-04-07 13:41:18 +0100

    Created CRUD beckend

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -1,20 +1,49 @@
 import Post from "../models/Post.js";
 
 export const getPosts = async (req,res) => {
-    const posts = await Post.find()
-    res.send([posts])
+    try {
+        const posts = await Post.find()
+        res.send([posts])
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
 }
 export const createPosts = async (req,res) => {
-    const {title , description} = req.body
-
-    const newPost = new Post ({title, description})
-    await newPost.save()
-
-    return res.json(newPost)
+    try {
+        const {title , description} = req.body
+        const newPost = new Post ({title, description})
+        await newPost.save()
+        return res.json(newPost)
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
 }
 
-export const updatingPosts = (req,res) => res.send('Updatin a post')
+export const updatingPosts = async (req,res) => {
+    try {
+        await Post.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        return res.send("received")
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+}
 
-export const deletingPosts = (req,res) => res.send('deleting a post')
+export const deletingPosts = async (req,res) => {
+    try {
+        const postRemoved = await Post.findByIdAndDelete(req.params.id)
+        if (!postRemoved) return res.sendStatus(404)
+        return res.send(204)
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+}
 
-export const getPostId = (req,res) => res.send('getting a post')
+export const getPostId = async (req,res) => {
+    try {
+        const postById = await Post.findById(req.params.id)
+        if (!postById) return res.sendStatus(404)
+        return res.json(postById)
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+}
--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -4,7 +4,7 @@ const router = Router()
 
 router.get('/posts', getPosts)
 router.post('/posts', createPosts )
-router.put('/posts', updatingPosts)
-router.delete('/posts', deletingPosts)
+router.put('/posts/:id', updatingPosts)
+router.delete('/posts/:id', deletingPosts)
 router.get('/posts/:id', getPostId )
 export default router
