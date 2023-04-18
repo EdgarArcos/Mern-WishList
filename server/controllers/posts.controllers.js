e824e9e3b7ac0268c51a6b00408a0ee313f148c6
Edgar Arcos <edgararcosmartin@gmail.com>
2023-04-18 17:04:07 +0100

    solventando problemas

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -3,19 +3,19 @@ import { uploadImage, deleteImage } from "../Library/cloudinary.js";
 import fs from "fs-extra";
 
 
-export const getPosts = async (req,res) => {
+export const getPosts = async (req, res) => {
     try {
         const posts = await Post.find()
-        res.send([posts])
+        res.send(posts)
     } catch (error) {
-        return res.status(500).json({message: error.message})
+        return res.status(500).json({ message: error.message })
     }
 }
-export const createPosts = async (req,res) => {
+export const createPosts = async (req, res) => {
     try {
-        const {title , description} = req.body
+        const { title, description } = req.body
         let image;
-        if (req.files) {
+        if (req.files?.image) {
             const result = await uploadImage(req.files.image.tempFilePath)
             image = {
                 url: result.secure_url,
@@ -24,40 +24,40 @@ export const createPosts = async (req,res) => {
             await fs.remove(req.files.image.tempFilePath)
         }
 
-        const newPost = new Post ({title, description, image})
+        const newPost = new Post({ title, description, image })
         await newPost.save()
         return res.json(newPost)
     } catch (error) {
-        return res.status(500).json({message: error.message})
+        return res.status(500).json({ message: error.message })
     }
 }
 
-export const updatingPosts = async (req,res) => {
+export const updatingPosts = async (req, res) => {
     try {
-        await Post.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        await Post.findByIdAndUpdate(req.params.id, req.body, { new: true })
         return res.send("received")
     } catch (error) {
-        return res.status(500).json({message: error.message})
+        return res.status(500).json({ message: error.message })
     }
 }
 
-export const deletingPosts = async (req,res) => {
+export const deletingPosts = async (req, res) => {
     try {
         const postRemoved = await Post.findByIdAndDelete(req.params.id)
         if (!postRemoved) return res.sendStatus(404)
-        if (postRemoved.image.public_id)await deleteImage(postRemoved.image.public_id)
+        if (postRemoved.image.public_id) await deleteImage(postRemoved.image.public_id)
         return res.sendStatus(204)
     } catch (error) {
-        return res.status(500).json({message: error.message})
+        return res.status(500).json({ message: error.message })
     }
 }
 
-export const getPostId = async (req,res) => {
+export const getPostId = async (req, res) => {
     try {
         const postById = await Post.findById(req.params.id)
         if (!postById) return res.sendStatus(404)
         return res.json(postById)
     } catch (error) {
-        return res.status(500).json({message: error.message})
+        return res.status(500).json({ message: error.message })
     }
 }
